08f168b51eed8628ab17dae88e0bb886f3b34e28
Antonio M. Amaya <amac@tid.es>
2014-09-15 13:38:32 +0200

    Added a script to build a (F)OTA

---
+++ build_fota.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source ./.config
+source ./.userconfig
+export VARIANT=user
+export MOZILLA_OFFICIAL=1
+
+export DEVICE=$PRODUCT_NAME
+export `grep MOZ_APP_VERSION ${GECKO_PATH}/b2g/confvars.sh`
+
+
+case $1 in
+  fota)
+        TYPE=fota
+        ./build_sto3.sh gecko-update-fota
+        SRC_FILE=`pwd`/out/target/product/${DEVICE}/fota-${DEVICE}-update.mar
+        ;;
+    *) TYPE=ota
+        ./build_sto3.sh gecko-update-full
+        SRC_FILE=${GECKO_OBJDIR}/dist/b2g-update/b2g-${DEVICE}-gecko-update.mar
+        ;;
+esac
+
+export `grep BuildID out/target/product/flame/system/b2g/platform.ini`
+
+echo "APP VERSION: ${MOZ_APP_VERSION}"
+echo "Build ID: ${BuildID}"
+
+
+FILE_OTA="${TYPE}-${DEVICE}-update.mar"
+URL="http://192.168.2.2:4576/ota/${FILE_OTA}"
+
+cp ${SRC_FILE} /var/www/ota/${FILE_OTA}
+
+export ANDROID_TOOLCHAIN=`pwd`/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin
+
+./tools/update-tools/build-update-xml.py -v 99.${MOZ_APP_VERSION} -V 99.${MOZ_APP_VERSION} -o /var/www/ota/update.xml -i ${BuildID} -u ${URL} /var/www/ota/${FILE_OTA}
+
