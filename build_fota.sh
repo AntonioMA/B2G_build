ce1c9b194e0c55035e4f3c14ab747357982a8e80
Antonio M. Amaya <amac@peregrin>
2014-09-18 10:42:05 +0200

    Small fota generation changes

--- build_fota.sh
+++ build_fota.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+if [ -f ./config_fota ]
+then
+  source ./config_fota
+else
+  DEST_DIR=/var/www/ota
+fi
+
 source ./.config
 source ./.userconfig
-export VARIANT=user
-export MOZILLA_OFFICIAL=1
+export VARIANT MOZILLA_OFFICIAL
 
 export DEVICE=$PRODUCT_NAME
 export `grep MOZ_APP_VERSION ${GECKO_PATH}/b2g/confvars.sh`
-
 
 case $1 in
   fota)
@@ -30,9 +35,9 @@ echo "Build ID: ${BuildID}"
 FILE_OTA="${TYPE}-${DEVICE}-update.mar"
 URL=`grep app.update.url gaia/profile/user.js | grep -v "//pref" | cut -d\" -f 4 | sed -e "s/update.xml/${FILE_OTA}/g"`
 
-cp ${SRC_FILE} /var/www/ota/${FILE_OTA}
+cp ${SRC_FILE} ${DEST_DIR}/${FILE_OTA}
 
 export ANDROID_TOOLCHAIN=`pwd`/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin
 
-./tools/update-tools/build-update-xml.py -v 99.${MOZ_APP_VERSION} -V 99.${MOZ_APP_VERSION} -o /var/www/ota/update.xml -i ${BuildID} -u ${URL} /var/www/ota/${FILE_OTA}
+./tools/update-tools/build-update-xml.py -v ${MOZ_APP_VERSION} -V ${MOZ_APP_VERSION} -o ${DEST_DIR}/update.xml -i ${BuildID} -u ${URL} ${DEST_DIR}/${FILE_OTA}
 
